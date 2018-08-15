f470a70af3c743730a80913c3dc412a8801e3593
Martin Panzer <postremus1996@googlemail.com>
2018-08-15 20:38:46 +0200

    fix docker image build add docker io credentials

--- .travis/build-and-deploy.sh
+++ .travis/build-and-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd ../docker
-docker build postremus/jenkins
+cd ./docker
+docker build . -t postremus/jenkins
 
 docker login -u $DOCKER_IO_USERNAME -p $DOCKER_IO_TOKEN
 docker push postremus/jenkins
